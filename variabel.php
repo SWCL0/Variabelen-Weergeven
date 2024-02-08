8e86036edcb0eb0a5bddaa987d892ffd51a66ae6
SWCL0 <stan.w.c@icloud.com>
2024-02-08 10:17:46 +0100

    Update variabel.php
    Opdracht afgemaakt:
    - echo toegevoegd
    - opdracht 2 eronder toegevoegd

--- variabel.php
+++ variabel.php
@@ -1,9 +1,19 @@
 <?php
-
+// OPDRACHT 1
+// Vaste variabelen
 $a = "Ik heb niet gefaald";
 $b = "die niet werken";
 $c = 10000;
 
+// In elkaar zetten en weergeven van zin
+echo $a . ". Ik heb alleen " . $c . " manieren gevonden " . $b . "." . PHP_EOL;
 
+// OPDRACHT 2
+// Vaste variabelen
+$d = "die geen fout";
+$e = "en";
+$f = "niet.";
 
+// In elkaar zetten en weergeven van zin
+echo "Mensen " . $d .  $e . " maken, werken " . $f;
 ?>
